1407bf2269674e1de75d3a58977b208f9e5f8f8e
AlzyWelzy <welzyalzy@gmail.com>
2022-12-27 18:18:10 +0530

    using all paradigms

--- tic_tac_toe/main-v2.py
+++ tic_tac_toe/main-v2.py
@@ -55,29 +55,31 @@ class Game:
         return True
 
     def main(self):
-        self.player1_name = input("Player 1, enter your name: ")
-        self.player2_name = input("Player 2, enter your name: ")
         while True:
+            self.player1_name = input("Player 1, enter your name: ")
+            self.player2_name = input("Player 2, enter your name: ")
             print(f"\nWelcome to Tic-Tac-Toe, {self.player1_name} and {self.player2_name}!\n")
             self.player1_symbol = input(f"{self.player1_name}, choose your symbol (X or O): ").upper()
             if self.player1_symbol == "X":
                 self.player2_symbol = "O"
             else:
                 self.player2_symbol = "X"
-            self.draw_board()
+            self.board = [["-" for i in range(3)] for j in range(3)]
             self.current_player = self.player1_name
             while True:
+                self.draw_board()
                 row, col = self.get_move(self.current_player)
                 if self.current_player == self.player1_name:
                     symbol = self.player1_symbol
                 else:
                     symbol = self.player2_symbol
                 self.board[row][col] = symbol
-                self.draw_board()
                 if self.has_won(symbol):
+                    self.draw_board()
                     print(f"\n{self.current_player} has won!")
                     break
                 if self.check_draw():
+                    self.draw_board()
                     print("\nGame is a draw!")
                     break
                 if self.current_player == self.player1_name:
