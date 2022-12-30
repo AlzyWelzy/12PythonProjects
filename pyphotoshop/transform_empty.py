27a94679daa3ced1ec6450e890d7ffb76bd5039e
AlzyWelzy <welzyalzy@gmail.com>
2022-12-30 22:16:31 +0530

    making it

--- pyphotoshop/transform_empty.py
+++ pyphotoshop/transform_empty.py
@@ -71,13 +71,41 @@ def apply_kernel(image, kernel):
     # [1 0 -1]
     # [2 0 -2]
     # [1 0 -1]
-    pass
+    # represents x, y pixels of image, # channels (R, G, B)
+    x_pixels, y_pixels, num_channels = image.array.shape
+    # making a new array to copy values to!
+    new_im = Image(x_pixels=x_pixels, y_pixels=y_pixels,
+                   num_channels=num_channels)
+    # this is a variable that tells us how many neighbors we actually look at (ie for a 3x3 kernel, this value should be 1)
+    neighbor_range = kernel.shape[0] // 2
+    for x in range(x_pixels):
+        for y in range(y_pixels):
+            for c in range(num_channels):
+                total = 0
+                for x_i in range(max(0, x-neighbor_range), min(new_im.x_pixels-1, x+neighbor_range)+1):
+                    for y_i in range(max(0, y-neighbor_range), min(new_im.y_pixels-1, y+neighbor_range)+1):
+                        x_k = x_i + neighbor_range - x
+                        y_k = y_i + neighbor_range - y
+                        kernel_val = kernel[x_k, y_k]
+                        total += image.array[x_i, y_i, c] * kernel_val
+                new_im.array[x, y, c] = total
+    return new_im
 
 
 def combine_images(image1, image2):
     # let's combine two images using the squared sum of squares: value = sqrt(value_1**2, value_2**2)
     # size of image1 and image2 MUST be the same
-    pass
+    # represents x, y pixels of image, # channels (R, G, B)
+    x_pixels, y_pixels, num_channels = image1.array.shape
+    # making a new array to copy values to!
+    new_im = Image(x_pixels=x_pixels, y_pixels=y_pixels,
+                   num_channels=num_channels)
+    for x in range(x_pixels):
+        for y in range(y_pixels):
+            for c in range(num_channels):
+                new_im.array[x, y, c] = (
+                    image1.array[x, y, c]**2 + image2.array[x, y, c]**2)**0.5
+    return new_im
 
 
 if __name__ == '__main__':
@@ -107,3 +135,15 @@ if __name__ == '__main__':
     # blur using kernel size of 15
     blur_15 = blur(city, 15)
     blur_15.write_image('blur_k15.png')
+
+    # let's apply a sobel edge detection kernel on the x and y axis
+    sobel_x = apply_kernel(city, np.array(
+        [[1, 2, 1], [0, 0, 0], [-1, -2, -1]]))
+    sobel_x.write_image('edge_x.png')
+    sobel_y = apply_kernel(city, np.array(
+        [[1, 0, -1], [2, 0, -2], [1, 0, -1]]))
+    sobel_y.write_image('edge_y.png')
+
+    # let's combine these and make an edge detector!
+    sobel_xy = combine_images(sobel_x, sobel_y)
+    sobel_xy.write_image('edge_xy.png')
